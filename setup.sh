3f79d5eab0a2999889cc4346defcc5f49c2ab02f
Sean Laurvick <smlaurvick@gmail.com>
2018-01-08 20:20:14 +0000

    Added .gitignore, trying with nethack 3.6

--- setup.sh
+++ setup.sh
@@ -10,10 +10,12 @@ sudo apt-get -y install git-core build-essential bison flex libncurses5-dev
 
 # Nethack
 echo "Installing NetHack..."
-git clone http://git.code.sf.net/p/nethack/NetHack
+# git clone http://git.code.sf.net/p/nethack/NetHack
+wget https://s3.amazonaws.com/nethack.org/3.6.0/nethack-360-src.tgz
+tar -xzvf nethack-360-src.tgz
 
-cp raspbian-jessie.hints NetHack/sys/unix/hints/raspbian-jesse
-cd NetHack/
+cp raspbian-jessie.hints nethack-3.6.0/sys/unix/hints/raspbian-jesse
+cd nethack-3.6.0/
 sh ./sys/unix/setup.sh sys/unix/hints/raspbian-jesse
 make all
 sudo make install
